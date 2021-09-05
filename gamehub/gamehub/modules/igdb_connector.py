37b88a4a01272d14b23f640eacf02ba889d94348
avram <avramneoko6@gmail.com>
2021-09-05 21:47:37 +0300

    IGDB integration fully done Created custom filter for reviews count Created List- and DetailView game pages

---
+++ gamehub/gamehub/modules/igdb_connector.py
@@ -0,0 +1,67 @@
+import os
+import requests
+from datetime import datetime, timedelta
+# Ну я в курсе, что, скорее всего, это нужно было в класс оборачивать
+# Но я не был уверен, что на каждый запрос стоит создавать экземпляр этого класса
+# Поэтому сделал вот таким модулем. Если это не правильно - нет проблем, переделаю
+
+
+def get_game_list(params):
+    return get_data_from_endpoint('https://api.igdb.com/v4/games/', params=params)
+
+
+def get_game(game_name, params):
+    # Значение параметра search нужно обязательно оборачивать в ""
+    params['search'] = f'"{game_name.replace("-", " ")}"'
+
+    response = get_data_from_endpoint('https://api.igdb.com/v4/games/', params=params)
+
+    if len(response) == 0:
+        return None
+
+    return response[0]
+
+
+def get_data_from_endpoint(endpoint, params):
+    # Преобразование словаря в строку нужного формата для запроса
+    data = '; '.join([f'{key} {value}' for key, value in params.items()]) + ';'
+
+    return requests.post(endpoint,
+                         headers=_get_headers(),
+                         data=data).json()
+
+
+def _get_token(client_id, client_secret):
+    params = {
+        'client_id': f'{client_id}',
+        'client_secret': f'{client_secret}',
+        'grant_type': 'client_credentials',
+    }
+
+    access_token, expires_in, _ = requests.post(f'https://id.twitch.tv/oauth2/token',
+                                                params=params).json().values()
+
+    return access_token, expires_in
+
+
+def _get_headers():
+    # Токен выдаётся почти на 2 месяца (в среднем). Не будем его запрашивать каждый раз
+    # Экономит секунду
+    global _current_token, _last_token_request, _expires_in
+
+    if not _current_token or not _last_token_request or \
+            _last_token_request + timedelta(seconds=_expires_in) < datetime.now():
+
+        _last_token_request = datetime.now()
+        _current_token, _expires_in = _get_token(os.environ.get('Client_id'),
+                                                 os.environ.get('Client_secret'))
+
+    return {
+        'Client-ID': os.environ.get('Client_id'),
+        'Authorization': f'Bearer {_current_token}'
+    }
+
+
+_last_token_request = None
+_expires_in = None
+_current_token = None
