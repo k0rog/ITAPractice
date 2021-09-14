c4737da678a3d7a265dc800f9ba25701f3a296d3
avram <avramenoko@mail.ru>
2021-09-14 17:30:04 +0300

    Forth phase done

--- gamehub/users/urls.py
+++ gamehub/users/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('registration', views.SignUpView.as_view(), name='registration'),
     path('authorization', views.SignInView.as_view(), name='authorization'),
     path('logout', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
-    # Since this page is available only to authorized users, I decided not to make a slug. Should I remake this?
     path('profile', views.UserProfileView.as_view(), name='profile_page'),
     path('my_musts', views.UserMustsView.as_view(), name='musts'),
-    path('musts', views.musts, name='musts_update')
+    path('musts', views.MustsView.as_view(), name='musts_update')
 ]
