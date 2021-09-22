02ec71ec707d13e11d31423df68a6d60a125848f
avram <avramneoko6@gmail.com>
2021-09-22 23:36:19 +0300

    .

---
+++ config/celery.py
@@ -0,0 +1,19 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('config')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'pulling-games-to-database': {
+        'task': 'gamehub.tasks.pull_games',
+        'schedule': 1000,
+        # 'schedule': 10.0,
+    }
+}
