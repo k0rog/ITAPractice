4ef5f4956cf6397357ff6f455adc4ca0bd27b59e
avram <avramneoko6@gmail.com>
2021-09-22 23:38:00 +0300

    .

--- config/settings.py
+++ config/settings.py
@@ -124,5 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CELERY_TIMEZONE = 'Europe/Minsk'
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = 'django-db'
