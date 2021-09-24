9e43d81b6dacf84ba9d865c9e100c15f7e121137
avram <avramneoko6@gmail.com>
2021-09-24 16:46:42 +0300

    Game list and detail endpoint done

---
+++ gamehub/rest/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from gamehub.models import Game
+
+
+class GameListSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+
+    class Meta:
+        model = Game
+        fields = ('id', 'name', 'slug', 'cover', 'genres',)
+
+
+class GameDetailSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    platforms = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+
+    class Meta:
+        model = Game
+        exclude = ('igdb_id',)
