64eab5ae2064c1d261833cbb834239a3f3faa2df
k0rog <avramneoko6@gmail.com>
2021-10-12 23:38:26 +0300

    Docker almost tuned

--- gamehub/management/commands/pull_games.py
+++ gamehub/management/commands/pull_games.py
@@ -1,27 +1,15 @@
 from django.core.management.base import BaseCommand
 from gamehub.utils.igdb_connector import IGDBWrapper
 from gamehub.models import Game, Platform, Genre, Screenshot
-from datetime import datetime
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print('-------------------------------------------------')
-        print('-------------------------------------------------')
-        print('-------------------------------------------------')
-        print('-------------------------------------------------')
-        print('-------------------------------------------------')
         igdb_api = IGDBWrapper()
 
-        print('Collecting games start')
-        total_start = datetime.now()
         games = igdb_api.get_game_list()[:5]
-        print(f'Game list received in {datetime.now()-total_start}')
 
-        print('Database pushing start')
-        games_pushing = datetime.now()
         for i, game in enumerate(games):
-            start = datetime.now()
             new_game, created = Game.objects.update_or_create(
                 igdb_id=game['id'],
                 defaults=game['defaults']
@@ -39,6 +27,3 @@ class Command(BaseCommand):
                 except KeyError:
                     platform, _ = Platform.objects.get_or_create(name=platform['name'])
                 new_game.platforms.add(platform)
-            print(f'    Game {i} pushed in {datetime.now() - start}')
-        print(f'Games pushed in {datetime.now() - games_pushing}')
-        print(f'Collecting games ended in {datetime.now() - total_start}')
